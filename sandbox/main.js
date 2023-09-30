79f335aa4652f3d14165e1773822528c1d342d3e
0xF7A4C6 <0xF7A4C6@proton.me>
2023-09-30 04:03:04 +0200

    edit fp

--- sandbox/main.js
+++ sandbox/main.js
@@ -1,43 +1,45 @@
 const { JSDOM, ResourceLoader } = require("jsdom");
 const fs = require('fs');
-
 const express = require('express');
 
 const app = express();
-const port = 1234;
+const port = process.argv[2];;
 
 app.use(express.json());
 
-// Create an array to hold multiple JSDOM contexts
+const NUM_CONTEXTS = 10;
 const jsdomContexts = [];
-const NUM_CONTEXTS = 2; // Adjust the number of contexts as needed
 
+const hswBindScript = fs.readFileSync(__dirname + "/hsw_bind.js", "utf-8");
+
+// Initialize JSDOM contexts
 for (let i = 0; i < NUM_CONTEXTS; i++) {
     const { window } = new JSDOM(``, {
-        contentType: "text/html",
         includeNodeLocations: false,
         runScripts: "dangerously",
-        pretendToBeVisual: true,
+        pretendToBeVisual: false,
         resources: new ResourceLoader({
             userAgent: `Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36`
         })
     });
+    window.eval(hswBindScript);
     jsdomContexts.push(window);
 }
 
-app.post('/n', (req, res) => {
+
+app.post('/n', async (req, res) => {
+    const startTime = performance.now();
     const { jwt: token } = req.body;
 
-    // Rotate through the JSDOM contexts
     const currentContext = jsdomContexts.shift();
     jsdomContexts.push(currentContext);
 
     try {
-        currentContext.eval(fs.readFileSync(__dirname + "/hsw_bind.js", "utf-8"));
-        currentContext.hsw(token).then(function (result) {
-            console.log('got', result.length)
-            res.send(result);
-        });
+        const result = await currentContext.hsw(token);
+
+        const endTime = performance.now();
+        console.log(`(took ${(endTime - startTime).toFixed(2)} ms, chars: ${result.length})`);
+        res.send(result);
     } catch (error) {
         console.error(error);
         res.status(401).json({ error: 'Invalid JWT' });
