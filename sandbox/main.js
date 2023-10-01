f29fc744ff763f108d80f276a2ae68c5fc9626da
0xF7A4C6 <0xF7A4C6@proton.me>
2023-10-01 09:07:16 +0200

    load fp from file

--- sandbox/main.js
+++ sandbox/main.js
@@ -2,6 +2,15 @@ const { JSDOM, ResourceLoader } = require("jsdom");
 const fs = require('fs');
 const express = require('express');
 
+const fileContent = fs.readFileSync('cleaned.txt', 'utf8');
+const lines = fileContent.split('\n').filter(line => line.trim() !== '');
+
+function get_fp() {
+    const randomIndex = Math.floor(Math.random() * lines.length);
+    const randomLine = lines[randomIndex];
+    return randomLine
+}
+
 const app = express();
 const port = process.argv[2];;
 
@@ -12,30 +21,29 @@ const jsdomContexts = [];
 
 const hswBindScript = fs.readFileSync(__dirname + "/hsw_bind.js", "utf-8");
 
-
 for (let i = 0; i < NUM_CONTEXTS; i++) {
     const { window } = new JSDOM(``, {
         includeNodeLocations: false,
         runScripts: "dangerously",
         pretendToBeVisual: false,
         resources: new ResourceLoader({
-            userAgent: `Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36`
+            userAgent: `Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36`
         })
     });
     window.eval(hswBindScript);
     jsdomContexts.push(window);
 }
 
-
 app.post('/n', async (req, res) => {
     const startTime = performance.now();
-    const { jwt: token } = req.body;
+    const { jwt: token, fp: fpb64 } = req.body;
 
     const currentContext = jsdomContexts.shift();
     jsdomContexts.push(currentContext);
 
     try {
-        const result = await currentContext.hsw(token);
+        let fp = get_fp() // fpb64
+        const result = await currentContext.hsw(token, fp);
 
         const endTime = performance.now();
         console.log(`(took ${(endTime - startTime).toFixed(2)} ms, chars: ${result.length})`);
